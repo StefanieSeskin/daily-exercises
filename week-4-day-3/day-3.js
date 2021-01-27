42a3504eca2543979e54d87338cb16ae14c01bd5
StefanieSeskin <stefanieaseskin@gmail.com>
2021-01-27 09:57:30 -0500

    foo

---
+++ week-4-day-3/day-3.js
@@ -0,0 +1,13 @@
+// Q1:
+// prevent myForm from refreshing the page on submit
+// if the value of the input is "multiply"
+// grab the value of #num1 and #num2
+// and show the answer in the input
+
+document.querySelector("#myform").addEventListener("submit", function(e)}
+e.preventDefault();
+
+
+// Q2:
+// update #box to the background color green when
+//myBtn is clicked
